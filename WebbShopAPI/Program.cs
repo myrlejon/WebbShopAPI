6e341e087f498f4c65cc618bfccc1bb2d15de0e5
myrlejon <60348075+myrlejon@users.noreply.github.com>
2021-03-08 15:20:17 +0100

    Add project files.

---
+++ WebbShopAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebbShopAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
